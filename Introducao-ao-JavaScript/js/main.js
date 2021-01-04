2779435f312485bc286f9567c99918d4031da480
MatheusTHM <thommesani@gmail.com>
2021-01-04 19:04:41 -0300

    Introdução ao JavaScript

---
+++ Introducao-ao-JavaScript/js/main.js
@@ -0,0 +1,52 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar!";
+}
+
+function trocar(elemento){
+    document.getElementById(elemento).innerHTML = "Mouseover";
+}
+
+function voltar(elemento){
+    document.getElementById(elemento).innerHTML = "Mouseout";
+}
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+alert soma(5, 10);
+*/
+
+// var d = new Date();
+// alert(d.getDay);
+// alert(d.getHours);
+// alert(d.getMinutes);
+
+
+//var idade = prompt("Qual sua idade?");
+
+/*
+var frutas = [{nome:"maçã", cor="vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(fruta[1].nome);
+*/
+
+// var lista = ["maçã", "pêra", "laranja"];
+// lista.push("uva");
+// lista.pop();
+
+// console.log(lista);
+// console.log(lista.toString());
+// console.log(lista.join(" | "));
+
+// var nome = "Matheus TH";
+// var n1 = 5;
+// var n2 = 3;
+// var frase = "Teste de replace";
+// alert(nome + " tem " + idade + " anos.");
+// alert(idade + idade2);
+// console.log(nome);
+// console.log(n1 * n2);
+// console.log(frase.toLocaleLowerCase());
+// alert(frase.replace("Replace", "Troca"));
